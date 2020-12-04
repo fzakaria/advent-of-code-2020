87eb86759b8a58f6f9d4264c77b82db675cc9d5c
Farid Zakaria <farid.m.zakaria@gmail.com>
2020-12-04 14:55:44 -0800

    First day done

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,9 @@
-use std::str::FromStr;
+use std::collections::HashSet;
+use std::error::Error;
 use std::iter::Iterator;
+use std::str::FromStr;
 use std::string::ToString;
+use advent_of_code_2020::UnsolvedError;
 
 #[derive(Debug, PartialEq)]
 struct RGB {
@@ -9,8 +12,50 @@ struct RGB {
     b: u8,
 }
 
-fn main() {
-	let input = include_str!("day1.txt");
-    input.lines().map( |line| line.to_string());
-    println!("Hello, world! {}", input);
+fn part1() -> Result<(), Box<dyn Error>> {
+    let expected_sum = 2020;
+    let input = include_str!("day1_1.txt");
+    let numbers: HashSet<u32> = input
+        .lines()
+        .flat_map::<Result<u32, _>, _>(|line| line.parse())
+        .collect();
+    for number in &numbers {
+        let to_find = expected_sum - number;
+        if numbers.contains(&to_find) {
+            println!("part1: {}", to_find * number);
+            return Ok(());
+        }
+    }
+
+    Err(Box::new(UnsolvedError))
+}
+
+
+fn part2() -> Result<(), Box<dyn Error>> {
+    let expected_sum = 2020;
+    let input = include_str!("day1_1.txt");
+    let numbers: HashSet<i32> = input
+        .lines()
+        .flat_map::<Result<i32, _>, _>(|line| line.parse())
+        .collect();
+    for number_a in &numbers {
+        let to_find_first = expected_sum - number_a;
+        for number_b in &numbers {
+            let to_find = to_find_first - number_b;
+            if numbers.contains(&to_find) {
+                println!("part2: {}", to_find * number_a * number_b);
+                return Ok(());
+            }
+        }
+    }
+
+    Err(Box::new(UnsolvedError))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // part 1
+    part1()?;
+
+    // part 2
+    return part2()
 }
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use std::fmt;
+use std::error::Error;
+
+type Result<T> = std::result::Result<T, UnsolvedError>;
+
+// Define our error types. These may be customized for our error handling cases.
+// Now we will be able to write our own errors, defer to an underlying error
+// implementation, or do something in between.
+#[derive(Debug, Clone)]
+pub struct UnsolvedError;
+
+impl fmt::Display for UnsolvedError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Could not solve this problem")
+    }
+}
+
+impl Error for UnsolvedError {}
