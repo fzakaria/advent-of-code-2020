9aa0ad099c0e7ff5704e3c0a0bcf6822ff30300d
Farid Zakaria <fmzakari@google.com>
2020-12-03 21:14:57 -0800

    Formatted

--- shell.nix
+++ shell.nix
@@ -1,11 +1,8 @@
 let
   sources = import ./nix/sources.nix;
   moz_overlay = import sources.nixpkgs-mozilla;
-  pkgs = import sources.nixpkgs { overlays = [moz_overlay]; };
-in
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.latest.rustChannels.nightly.rust
-  ];
+  pkgs = import sources.nixpkgs { overlays = [ moz_overlay ]; };
+in pkgs.mkShell {
+  buildInputs = [ pkgs.latest.rustChannels.nightly.rust ];
   RUST_BACKTRACE = 1;
 }
